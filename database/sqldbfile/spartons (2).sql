01f8a4d38542ec8b43524990b6a053d1f68e63a5
amiraligull <amirgull42@gmail.com>
2024-10-06 11:40:30 +0500

    db yodated

--- database/sqldbfile/spartons (2).sql
+++ database/sqldbfile/spartons (2).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 05, 2024 at 03:19 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Oct 06, 2024 at 08:39 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `spartonecom`
+-- Database: `spartons`
 --
 
 -- --------------------------------------------------------
@@ -97,6 +97,13 @@ CREATE TABLE `carts` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `carts`
+--
+
+INSERT INTO `carts` (`id`, `user_id`, `product_id`, `quantity`, `created_at`, `updated_at`) VALUES
+(7, 2, 5, 1, '2024-10-05 08:51:18', '2024-10-05 08:51:18');
 
 -- --------------------------------------------------------
 
@@ -210,6 +217,14 @@ CREATE TABLE `orders` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `name`, `email`, `phone`, `address`, `subtotal`, `discount`, `total`, `status`, `created_at`, `updated_at`) VALUES
+(6, 2, 'amir gull', 'amirgull42@gmail.com', '03023730715', 'Grass Mandi,H#7 St#7 Mohallah Data Colony Multan.', 7, '0', 7, NULL, '2024-10-05 08:33:13', '2024-10-05 08:33:13'),
+(7, 2, 'amir gull', 'amirgull42@gmail.com', '03023730715', 'Grass Mandi,H#7 St#7 Mohallah Data Colony Multan.', 7, '1.05', 6, NULL, '2024-10-05 08:44:32', '2024-10-05 08:44:32');
+
 -- --------------------------------------------------------
 
 --
@@ -226,6 +241,14 @@ CREATE TABLE `order_items` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `price`, `total`, `created_at`, `updated_at`) VALUES
+(5, 6, 5, 1, 7, NULL, '2024-10-05 08:33:13', '2024-10-05 08:33:13'),
+(6, 7, 5, 1, 7, NULL, '2024-10-05 08:44:32', '2024-10-05 08:44:32');
 
 -- --------------------------------------------------------
 
@@ -452,7 +475,7 @@ ALTER TABLE `blog_posts`
 -- AUTO_INCREMENT for table `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -482,13 +505,13 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
